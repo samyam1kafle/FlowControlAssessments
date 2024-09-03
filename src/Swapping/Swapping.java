264210743077e374be171a23a98bf42d084dffd3
samyam <samyamkafle46@gmail.com>
2024-09-03 15:23:26 -0400

    Updates in swapping programs

--- src/Swapping/Swapping.java
+++ src/Swapping/Swapping.java
@@ -9,16 +9,16 @@ package Swapping;
 public class Swapping {
     public static void main(String[] args) {
       String seperator = "*". repeat(30);
-      swapTwoNumbersUsingTemp(7,9);
-      System.out.println(seperator);
-      swapTwoNumbersWithoutUsingTemp(4,3);
-      System.out.println(seperator);
-      swapThreeNumbersUsingTemp(5,6,7);
-      System.out.println(seperator);
-      swapThreeNumbersWithoutUsingTemp(1,2,3);
-      System.out.println(seperator);
-      swapFourNumbersUsingTemp(1,2,3,4);
-      System.out.println(seperator);
+//      swapTwoNumbersUsingTemp(1,2);
+//      System.out.println(seperator);
+//      swapTwoNumbersWithoutUsingTemp(1,2);
+//      System.out.println(seperator);
+//      swapThreeNumbersUsingTemp(1,2,3);
+//      System.out.println(seperator);
+//      swapThreeNumbersWithoutUsingTemp(1,2,3);
+//      System.out.println(seperator);
+//      swapFourNumbersUsingTemp(1,2,3,4);
+//      System.out.println(seperator);
       swapFourNumbersWithoutUsingTemp(1,2,3,4);
       System.out.println(seperator);
 
@@ -26,10 +26,10 @@ public class Swapping {
     // Program to swap 2 numbers using a temp variable
     private static void swapTwoNumbersUsingTemp(int a, int b) {
         System.out.println("Swapping two numbers with temp variable");
-
         System.out.println("Value of A : "+ a);
         System.out.println("Value of B : "+ b);
         System.out.println("-------------------------------");
+
         int temp = a;
         a = b;
         b = temp;
@@ -44,9 +44,10 @@ public class Swapping {
         System.out.println("Value of B : "+ b);
         System.out.println("-------------------------------");
 
-        a =  a + b ;
-        b = a - b;
-        a = a - b;
+
+        a =  a + b ; // 1 + 2 => 3 -> originally a = 1
+        b = a - b; // a = 3 - 2 => 1 -> b
+        a = a - b; // 3 - 1 => 2 -> a
 
         System.out.println("Value of A : "+ a);
         System.out.println("Value of B : "+ b);
@@ -60,10 +61,10 @@ public class Swapping {
         System.out.println("Value of B : "+ b);
         System.out.println("Value of C : "+ c);
         System.out.println("-------------------------------");
-        int temp = a;
-        a = b;
-        b = c;
-        c= temp;
+        int temp = a; // temp -> a -> 1
+        a = b; // a -> 2
+        b = c; // b -> 3
+        c= temp; // c -> 1
 
         System.out.println("Value of A : "+ a);
         System.out.println("Value of B : "+ b);
@@ -77,11 +78,20 @@ public class Swapping {
         System.out.println("Value of B : "+ b);
         System.out.println("Value of C : "+ c);
         System.out.println("-------------------------------");
+/*
+* c = 1
+* b = 3
+* a = 2
+* */
+        c =  a + b + c ; // c -> 6
+        b = c - ( b + a ); // 6 - 3 => 3 -> b
+        a = c - (b + a ); // 6 - (3 + 1) -> 2 -> a
+        c = c - (b + a); // 6 - (3+2) -> 1 ->c
 
-        c =  a + b + c ;
-        b = c - ( b + a );
-        a = c - (b + a );
-        c = c - (b + a);
+//        a = a+b+c;
+//        b = a -(b+c);
+//        c = a -(b+c);
+//        a = a -(b+c);
 
         System.out.println("Value of A : "+ a);
         System.out.println("Value of B : "+ b);
@@ -97,12 +107,17 @@ public class Swapping {
         System.out.println("Value of C : "+ c);
         System.out.println("Value of D : "+ d);
         System.out.println("-------------------------------");
-        int temp = a;
-        a = b;
-        b = c;
-        c= d;
-        d = temp;
+        int temp = a; // temp -> a -> 1
+        a = b; // a -> 2
+        b = c; // b -> 3
+        c = d; // c -> 4
+        d = temp; // d -> 1
 
+//        int temp1 = d;
+//        d = c;
+//        c = b;
+//        b = a;
+//        a = temp1;
         System.out.println("Value of A : "+ a);
         System.out.println("Value of B : "+ b);
         System.out.println("Value of C : "+ c);
@@ -118,12 +133,17 @@ public class Swapping {
         System.out.println("Value of C : "+ c);
         System.out.println("Value of D : "+ d);
         System.out.println("-------------------------------");
-
-        d =  a + b + c + d ;
-        c = d - (a + b + c);
-        b = d - (a + b + c);
-        a = d - (a + b + c);
-        d = d - (a + b + c);
+/*
+* c = 4
+* b = 3
+* a = 2
+* d = 1
+* */
+        d =  a + b + c + d ; // d -> (1+2+3+4) => 10
+        c = d - (a + b + c); // c -> 10 - (1+2+3) => 4
+        b = d - (a + b + c); // b -> 10 - (1+2+4) => 3
+        a = d - (a + b + c); // a -> 10 - (1+3+4) => 2
+        d = d - (a + b + c); // d -> 10 - (2+3+4) => 1
 
         System.out.println("Value of A : "+ a);
         System.out.println("Value of B : "+ b);
